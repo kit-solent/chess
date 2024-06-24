588dc384a24a6fde6695510704ce1baaaf26f2ca
kit-solent <nelsonmaxlang@gmail.com>
2024-06-24 14:15:48 +1200

    Add autoload script, start scene, and various placeholder functions.

---
+++ Core.gd
@@ -0,0 +1,7 @@
+extends Node
+
+func ip_to_code(ip:String):
+	pass
+
+func code_to_ip(Code:String):
+	pass
---
+++ start.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+func _on_join_button_down():
+	pass # Replace with function body.
+
+func _on_host_button_down():
+	pass # Replace with function body.
+
+func _on_join_code_text_changed(new_text):
+	pass # Replace with function body.
