ec0606cbd3107cb1775a164e1898bb6c0d683658
nelsonmaxlang@gmail.com <nelsonmaxlang@gmail.com>
2024-07-12 12:08:32 +1200

    Add start screen with title animation. Theme to be edited as needed.

--- start.gd
+++ start.gd
@@ -1,7 +1,38 @@
 extends Control
 
 func _ready():
-	pass
+	var godot_tween=get_tree().create_tween()
+	var chess_tween=get_tree().create_tween()
+	var loop_time=1
+
+	godot_tween.set_loops() # no arguments means run forever.
+	chess_tween.set_loops()
+
+	# Both back
+	godot_tween.tween_property($v_box_container/head/row/control/godot,"modulate",Color($v_box_container/head/row/control/godot.modulate,0.0),loop_time)
+	chess_tween.tween_property($v_box_container/head/row/control/chess,"modulate",Color($v_box_container/head/row/control/chess.modulate,0.0),loop_time)
+
+	# Godot forward, chess stays back another loop
+	godot_tween.tween_property($v_box_container/head/row/control/godot,"modulate",Color($v_box_container/head/row/control/godot.modulate,1.0),loop_time)
+	chess_tween.tween_property($v_box_container/head/row/control/chess,"modulate",Color($v_box_container/head/row/control/chess.modulate,0.0),loop_time)
+
+	# Chess forward, Godot stays forward another loop
+	godot_tween.tween_property($v_box_container/head/row/control/godot,"modulate",Color($v_box_container/head/row/control/godot.modulate,1.0),loop_time)
+	chess_tween.tween_property($v_box_container/head/row/control/chess,"modulate",Color($v_box_container/head/row/control/chess.modulate,1.0),loop_time)
+
+	# Godot back, Chess stays forward another loop
+	godot_tween.tween_property($v_box_container/head/row/control/godot,"modulate",Color($v_box_container/head/row/control/godot.modulate,0.0),loop_time)
+	chess_tween.tween_property($v_box_container/head/row/control/chess,"modulate",Color($v_box_container/head/row/control/chess.modulate,1.0),loop_time)
+
+	# then back to the top of the loop
+
+	# # Godot forward, Chess back
+	# godot_tween.tween_property($v_box_container/head/row/control/godot,"modulate",Color($v_box_container/head/row/control/godot.modulate,1.0),loop_time)
+	# chess_tween.tween_property($v_box_container/head/row/control/chess,"modulate",Color($v_box_container/head/row/control/chess.modulate,0.0),loop_time)
+
+	# # Chess forward, Godot back
+	# chess_tween.tween_property($v_box_container/head/row/control/chess,"modulate",Color($v_box_container/head/row/control/chess.modulate,1.0),loop_time)
+	# godot_tween.tween_property($v_box_container/head/row/control/godot,"modulate",Color($v_box_container/head/row/control/godot.modulate,0.0),loop_time)
 
 func _process(delta):
 	pass
