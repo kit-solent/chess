8aadc8544c5bc355895dfbe6b7978a8136f1d47b
nelsonmaxlang@gmail.com <nelsonmaxlang@gmail.com>
2024-07-22 17:42:30 +1200

    start join game screen

--- start.gd
+++ start.gd
@@ -32,11 +32,68 @@ func _ready():
 	godot(godot_tween,loop_time,true)
 	chess(chess_tween,loop_time,true)
 
-@onready var box1=%join_code_field/line_edit
+@onready var box1=%join_code_field/line_edit1
+@onready var box2=%join_code_field/line_edit2
+@onready var box3=%join_code_field/line_edit3
+@onready var box4=%join_code_field/line_edit4
+
+var current_code=""
 func _process(delta):
 	#if get_viewport().gui_get_focus_owner() is box1:
 	#	pass
 	pass
+
+const CODE_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+func fix_text(text):
+	var new = ""
+	for i in text:
+		if i in CODE_CHARS:
+			new += i.to_lower()
+		else:
+			$main/body/body/join/v_box_container/label.text = "Join code must contain only letters."
+	return new
+
+func _on_line_edit_1_text_changed(new_text):
+	var text = new_text.split(" ")
+	if len(text) > 1:
+		# pass on all text after the first space to the next box
+		_on_line_edit_2_text_changed("".join(text.slice(1)))
+		%join_code_field/line_edit2.grab_focus()
+	%join_code_field/line_edit1.text = fix_text(text[0])
+
+func _on_line_edit_2_text_changed(new_text):
+	var text = new_text.split(" ")
+	if len(text) > 1:
+		# pass on all text after the first space to the next box
+		_on_line_edit_2_text_changed("".join(text.slice(1)))
+		%join_code_field/line_edit3.grab_focus()
+	%join_code_field/line_edit2.text = fix_text(text[0])
+
+func _on_line_edit_3_text_changed(new_text):
+	var text = new_text.split(" ")
+	if len(text) > 1:
+		# pass on all text after the first space to the next box
+		_on_line_edit_2_text_changed("".join(text.slice(1)))
+		%join_code_field/line_edit4.grab_focus()
+	%join_code_field/line_edit3.text = fix_text(text[0])
+
+func _on_line_edit_4_text_changed(new_text):
+	%join_code_field/line_edit3.text = fix_text(new_text)
+
+
+func _on_line_edit_1_focus_exited():
+	pass
+	#if %join_code_field/line_edit1.text.to_lower() in Co
+
+func _on_line_edit_2_focus_exited():
+	pass # Replace with function body.
+
+func _on_line_edit_3_focus_exited():
+	pass # Replace with function body.
+
+func _on_line_edit_4_focus_exited():
+	pass # Replace with function body.
+
 
 # Title animation stuff
 func godot(tween,time,forward=true):
@@ -48,7 +105,7 @@ func chess(tween,time,forward=true):
 
 func _on_join_button_down():
 	$main/body/body.current_tab=2
-	%join_code_field/line_edit.grab_focus()
+	%join_code_field/line_edit1.grab_focus()
 
 func _on_host_button_down():
 	$main/body/body.current_tab=1
@@ -64,7 +121,7 @@ func _on_about_meta_clicked(meta):
 func _on_about_button_down():
 	pass # Replace with function body.
 
-const ALLOWED_CHARS="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123567890 "
+const ALLOWED_CHARS="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123567890 _-."
 func _on_line_edit_text_changed(new_text:String):
 	var pos=%username_lineedit.caret_column
 	var allowed=""
@@ -86,3 +143,4 @@ func _on_time_controls_enabled_button_down():
 		%time_controls_enabled.text="Enabled"
 	else:
 		%time_controls_enabled.text="No Time Controls"
+
