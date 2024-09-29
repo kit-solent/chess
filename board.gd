d2ee08ede36354fe2be6ec86a37cf29896901032
nelsonmaxlang@gmail.com <nelsonmaxlang@gmail.com>
2024-09-29 20:42:31 +1300

    change board state logic

--- board.gd
+++ board.gd
@@ -1,15 +1,15 @@
 extends Control
 
 @onready var grid = $panel_container/aspect_ratio_container/grid_container
-
+var board = GameState.new()
 
 func _ready():
 	# even after flipping the board the id's will be consistant accross players.
 	for i in 64:
 		var new = load("res://tile.tscn").instantiate()
 		@warning_ignore("integer_division")
-		new.set_bg((i - i / 8) % 2 == 0)
-		new.clicked.connect(tile_clicked.bind(i))
+		new.set_bg((i - i / 8) % 2 == 0) #                   / is floordiv by default. How un-pythonic.
+		new.clicked.connect(tile_clicked.bind(Vector2i(i%8, i/8)))
 		grid.add_child(new)
 
 	var index = 0
@@ -47,7 +47,7 @@ func _process(delta):
 	if Input.is_action_just_pressed("escape"):
 		(
 			$panel_container/aspect_ratio_container/grid_container
-			. get_children()[tile_id_to_index(selected_tile)]
+			. get_children()[tile_pos_to_index(selected_tile)]
 			. deselect()
 		)
 		selected_tile = null
@@ -56,61 +56,36 @@ func _process(delta):
 var selected_tile = null
 
 
-func tile_clicked(tile_id):
-	print("tile clicked: " + str(tile_id))
+func tile_clicked(tile_position:Vector2i):
+	print("tile clicked: " + str(tile_position))
 	if selected_tile:
-		move_piece.rpc(selected_tile, tile_id)
+		move_piece.rpc(selected_tile, tile_position)
 		(
 			$panel_container/aspect_ratio_container/grid_container
-			. get_children()[tile_id_to_index(tile_id)]
+			. get_children()[tile_pos_to_index(tile_position)]
 			. deselect()
 		)
 		selected_tile = null
 	elif (
 		$panel_container/aspect_ratio_container/grid_container
-		. get_children()[tile_id_to_index(tile_id)]
+		. get_children()[tile_pos_to_index(tile_position)]
 		. has_piece()
-	):
+	): # < whats this guy sad about?
 		(
 			$panel_container/aspect_ratio_container/grid_container
-			. get_children()[tile_id_to_index(tile_id)]
+			. get_children()[tile_pos_to_index(tile_position)]
 			. select()
 		)
-		selected_tile = tile_id
+		selected_tile = tile_position
 
 
-func tile_id_to_index(id: int):
+func tile_pos_to_index(pos: Vector2i):
 	if not Core.playing_as_white:
-		return 63 - id
+		return 63 - (pos.y*8 + pos.x)
 	else:
-		return id
+		return pos.y*8 + pos.x
 
 
 @rpc("call_local", "any_peer")
-func move_piece(from, to):
-	var p = (
-		$panel_container/aspect_ratio_container/grid_container
-		. get_children()[tile_id_to_index(from)]
-		. piece
-	)
-	(
-		$panel_container/aspect_ratio_container/grid_container
-		. get_children()[tile_id_to_index(from)]
-		. set_piece("")
-	)
-	(
-		$panel_container/aspect_ratio_container/grid_container
-		. get_children()[tile_id_to_index(to)]
-		. set_piece(p)
-	)
-
-	(
-		$panel_container/aspect_ratio_container/grid_container
-		. get_children()[tile_id_to_index(from)]
-		. deselect()
-	)
-	(
-		$panel_container/aspect_ratio_container/grid_container
-		. get_children()[tile_id_to_index(to)]
-		. deselect()
-	)
+func move_piece(from:Vector2i, to:Vector2i):
+	board.perform_move(from, to)
--- resources/GameState.gd
+++ resources/GameState.gd
@@ -5,20 +5,8 @@ class_name GameState extends Resource
 # for board positions they will use this convention. Whenever chess coords
 # e.g. e4 are used they will follow chess direction conventions.  
 
-signal checkmate(winner)
-signal draw(reason)
 
-var DEFAULT_BOARD = [
-	[PIECES.BLACK_ROOK  ,PIECES.BLACK_KNIGHT,PIECES.BLACK_BISHOP,PIECES.BLACK_QUEEN ,PIECES.BLACK_KING  ,PIECES.BLACK_BISHOP,PIECES.BLACK_KNIGHT,PIECES.BLACK_ROOK  ],
-	[PIECES.BLACK_PAWN  ,PIECES.BLACK_PAWN  ,PIECES.BLACK_PAWN  ,PIECES.BLACK_PAWN  ,PIECES.BLACK_PAWN  ,PIECES.BLACK_PAWN  ,PIECES.BLACK_PAWN  ,PIECES.BLACK_PAWN  ],
-	[PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE],
-	[PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE],
-	[PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE],
-	[PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE,PIECES.EMPTY_SQUARE],
-	[PIECES.WHITE_PAWN  ,PIECES.WHITE_PAWN  ,PIECES.WHITE_PAWN  ,PIECES.WHITE_PAWN  ,PIECES.WHITE_PAWN  ,PIECES.WHITE_PAWN  ,PIECES.WHITE_PAWN  ,PIECES.WHITE_PAWN  ],
-	[PIECES.WHITE_ROOK  ,PIECES.WHITE_KNIGHT,PIECES.WHITE_BISHOP,PIECES.WHITE_QUEEN ,PIECES.WHITE_KING  ,PIECES.WHITE_BISHOP,PIECES.WHITE_KNIGHT,PIECES.WHITE_ROOK  ],]
-var board = DEFAULT_BOARD
-enum PIECES  {
+enum PIECES {
 	EMPTY_SQUARE,
 	WHITE_KING,
 	WHITE_QUEEN,
@@ -32,20 +20,18 @@ enum PIECES  {
 	BLACK_KNIGHT,
 	BLACK_BISHOP,
 	BLACK_PAWN}
-var WHITE_PIECES = [
-	PIECES.WHITE_KING,
-	PIECES.WHITE_QUEEN,
-	PIECES.WHITE_ROOK,
-	PIECES.WHITE_KNIGHT,
-	PIECES.WHITE_BISHOP,
-	PIECES.WHITE_PAWN]
-var BLACK_PIECES = [
-	PIECES.BLACK_KING,
-	PIECES.BLACK_QUEEN,
-	PIECES.BLACK_ROOK,
-	PIECES.BLACK_KNIGHT,
-	PIECES.BLACK_BISHOP,
-	PIECES.BLACK_PAWN]
+
+var DEFAULT_BOARD = [
+	[PIECES.BLACK_ROOK  , PIECES.BLACK_KNIGHT, PIECES.BLACK_BISHOP, PIECES.BLACK_QUEEN , PIECES.BLACK_KING  , PIECES.BLACK_BISHOP, PIECES.BLACK_KNIGHT, PIECES.BLACK_ROOK  ],
+	[PIECES.BLACK_PAWN  , PIECES.BLACK_PAWN  , PIECES.BLACK_PAWN  , PIECES.BLACK_PAWN  , PIECES.BLACK_PAWN  , PIECES.BLACK_PAWN  , PIECES.BLACK_PAWN  , PIECES.BLACK_PAWN  ],
+	[PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE],
+	[PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE],
+	[PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE],
+	[PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE, PIECES.EMPTY_SQUARE],
+	[PIECES.WHITE_PAWN  , PIECES.WHITE_PAWN  , PIECES.WHITE_PAWN  , PIECES.WHITE_PAWN  , PIECES.WHITE_PAWN  , PIECES.WHITE_PAWN  , PIECES.WHITE_PAWN  , PIECES.WHITE_PAWN  ],
+	[PIECES.WHITE_ROOK  , PIECES.WHITE_KNIGHT, PIECES.WHITE_BISHOP, PIECES.WHITE_QUEEN , PIECES.WHITE_KING  , PIECES.WHITE_BISHOP, PIECES.WHITE_KNIGHT, PIECES.WHITE_ROOK  ],]
+
+var board = DEFAULT_BOARD
 
 
 func pos2vector(pos:String):
@@ -55,195 +41,26 @@ func pos2vector(pos:String):
 func vector2pos(vector:Vector2i):
 	return "abcdefgh"[vector.x]+"87654321"[vector.y]
 
-var entire_board
-
-var single_adjacents
-var single_diagonals
-var long_adjacents
-var long_diagonals
-
-var king_moves
-var queen_moves
-var rook_moves
-var knight_moves
-var bishop_moves
-var pawn_moves
-
-var moves
-
-func init():
-	single_adjacents = [
-		Vector2i.UP,Vector2i.DOWN,Vector2i.LEFT,Vector2i.RIGHT
-	]
-	
-	single_diagonals = [
-		Vector2i.UP + Vector2i.RIGHT,
-		Vector2i.DOWN, Vector2i.RIGHT,
-		Vector2i.DOWN + Vector2i.LEFT,
-		Vector2i.UP + Vector2i.LEFT
-	]
-	
-	long_diagonals = []
-	long_adjacents = []
-	
-	for i in 8: # 8 squares in every direction is enough to cover the whole board.
-		for a in single_diagonals:
-			long_diagonals.append(a*(i+1))
-		
-		for a in single_adjacents:
-			long_adjacents.append(a*(i+1))
-	
-	king_moves   = single_adjacents
-	queen_moves  = long_adjacents + long_diagonals
-	rook_moves   = long_adjacents
-	bishop_moves = long_diagonals
-	knight_moves = [
-		Vector2i(-2,-1),
-		Vector2i(-2, 1),
-		Vector2i(-1,-2),
-		Vector2i(-1, 2),
-		Vector2i( 2,-1),
-		Vector2i( 2, 1),
-		Vector2i( 1,-2),
-		Vector2i( 1, 2)
-	]
-	
-	pawn_moves = [
-		Vector2.UP,
-		2*Vector2.UP # Pawns can move 2 squares on their first move.
-	]
-	
-	moves = {
-		PIECES.WHITE_KING: king_moves,
-		PIECES.BLACK_KING: king_moves,
-		PIECES.WHITE_QUEEN: queen_moves,
-		PIECES.BLACK_QUEEN: queen_moves,
-		PIECES.WHITE_ROOK: rook_moves,
-		PIECES.BLACK_ROOK: rook_moves,
-		PIECES.WHITE_BISHOP: bishop_moves,
-		PIECES.BLACK_BISHOP: bishop_moves,
-		PIECES.WHITE_KNIGHT: knight_moves,
-		PIECES.BLACK_KNIGHT: knight_moves,
-		PIECES.WHITE_PAWN: pawn_moves,
-		PIECES.BLACK_PAWN: pawn_moves
-	}
-	
-	entire_board = []
-	for x in range(1,9):
-		for y in range(1,9):
-			entire_board.append(Vector2i(x,y))
+func fliped():
+	var x=board
+	x.reverse()
+	return x
 
 func perform_move(from:Vector2i, to:Vector2i):
 	"""
 	Moves the piece at square "from" to square "to" regardless of legal moves.
+	If the piece is a pawn moving to its final rank then promote it to a queen.
 	"""
-	board[to.y][to.x] = board[from.y][from.x]
+	if board[from.y][from.x] == PIECES.WHITE_PAWN and to.y == 0:
+		# if the piece is a white pawn moving to its final rank replace it with a queen.
+		board[to.y][to.x] = PIECES.WHITE_QUEEN
+	elif board[from.y][from.x] == PIECES.BLACK_PAWN and to.y == 8:
+		# same with a black one.
+		board[to.y][to.x] = PIECES.BLACK_QUEEN
+	else:
+		# otherwise keep the piece as is.
+		board[to.y][to.x] = board[from.y][from.x]
+	
+	# clear the "from" square to avoid duplicating the piece.
 	board[from.y][from.x] = PIECES.EMPTY_SQUARE
 
-func make_move_if_legal(start:Vector2i, stop:Vector2i):
-	pass
-
-func set_intersection(set1:Array, set2:Array) -> Array:
-	"""
-	Simulates the set intersection operator with arrays.
-	"""
-	var new=[]
-	for i in set1:
-		if i in set2:
-			new.append(i)
-	return new
-
-
-
-
-func is_legal_move(_from:String,_to:String):
-	var from = pos2vector(_from)
-	var to = pos2vector(_to)
-	#                 7-from.y is used because the board is stored from top to bottom and the y values go from bottom to top.
-	var piece = board[7-from.y][from.x]
-	var potential_moves = moves[piece] # an array of potential moves relative to
-	
-	# eliminate moves that are off the board.
-	potential_moves = set_intersection(potential_moves,entire_board)
-	
-	# eliminate moves blocked by allied pieces.
-	for y in board:
-		for x in y:
-			if board[y][x]:pass
-
-
-func all_legal_moves(_position:String, consider_checks = true):
-	var pos = pos2vector(_position)
-	var piece = board[pos.y][pos.x]
-	var pot_moves = []
-	for i in moves[piece]:
-		pot_moves.append(i + pos) # get an array of all potential moves relative to the pieces current position.
-	pot_moves = set_intersection(pot_moves, entire_board)
-	
-	if piece == PIECES.EMPTY_SQUARE:
-		return [] # no legal moves for an empty square
-	
-	if piece in [PIECES.WHITE_KING, PIECES.BLACK_KING]:
-		var _rtrn = []
-		for i in pot_moves:
-			# Kings can never move to a square with a friendly piece on it.
-			if piece == PIECES.WHITE_KING:
-				if board[i.y][i.x] in WHITE_PIECES:
-					continue
-			elif piece == PIECES.BLACK_KING:
-				if board[i.y][i.x] in BLACK_PIECES:
-					continue
-			
-			if consider_checks:
-				var test_check = duplicate() # make a copy of this board
-				test_check.perform_move(pos, i) # force the move
-				if test_check.is_check(): # and see if the result is in check
-					continue
-			
-			_rtrn.append(i)
-
-
-
-
-
-func is_check():
-	# first check the white king.
-	var white_king_pos = find(PIECES.WHITE_KING)[0] # there should only ever be one.
-	for i in get_pieces_by_colour(false):
-		# if the position of the king is in the legal moves of any enemy piece.
-		if white_king_pos in all_legal_moves(i,false):
-			return true
-	
-	# then check the black king.
-	var black_king_pos = find(PIECES.BLACK_KING)[0] # there should only ever be one.
-	for i in get_pieces_by_colour(true):
-		# if the position of the king is in the legal moves of any enemy piece.
-		if black_king_pos in all_legal_moves(i,false):
-			return true
-
-func get_pieces_by_colour(white:bool):
-	"""
-	Returns an array of all black pieces on the board.
-	"""
-	var pieces = []
-	var list_pieces
-	if white:
-		list_pieces = WHITE_PIECES
-	else:
-		list_pieces = BLACK_PIECES
-	for y in board:
-		for x in y:
-			if board[y][x] in list_pieces:
-				pieces.append(Vector2i(x,y))
-	return pieces
-
-func find(piece):
-	"""
-	Returns the positions of all pieces of the given type on the board.
-	"""
-	var pieces = []
-	for y in board:
-		for x in y:
-			if board[y][x] == piece:
-				pieces.append(Vector2i(x,y))
-	return pieces
